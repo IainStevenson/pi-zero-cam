0f7231c90d4c19506f5a8a9063c86cd23364a1f3
Iain Stevenson <iainstevenson@users.noreply.gitlab.com>
2026-03-08 18:51:35 +0000

    modified streaming service 1280*720*30 fps @1.5Mbps

--- start-streaming.sh
+++ start-streaming.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rpicam-vid -t 0 --width 640 --height 360 --exposure long --awb cloudy --framerate 30 --bitrate 1500000 --inline --listen -o tcp://0.0.0.0:8554
+rpicam-vid -t 0 --width 1280 --height 720 --exposure long --awb cloudy --framerate 30 --bitrate 1500000 --inline --listen -o tcp://0.0.0.0:8554
